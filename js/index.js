55ba043cfc399e6efdfd15f0882b6677da928920
Dario <dario.rupcic2003@gmail.com>
2023-02-14 17:26:43 +0100

    Color Sensitive to mouse position

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-alert('this is an alert.');
+// alert('this is an alert.');
 console.log('this is a log.');
-console.assert(2>4, 'this prints itself out as a warning');
+// console.assert(2>4, 'this prints itself out as a warning');
+
+document.querySelector('*').addEventListener('mousemove',(e) =>{
+    document.querySelector('*').style.background = `linear-gradient(rgb(${e.offsetX},20,20),rgb(${e.offsetY},20,20))`;
+    document.querySelector('*').style.backgroundRepeat = 'no-repeat';
+    console.log(document.querySelector('*').tagName)
+})
